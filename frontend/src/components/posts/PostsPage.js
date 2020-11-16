aca15604335f54f672ddd56395165f6f787773be
JeremyLavigne <lavigne.jeremy@post.com>
2020-11-16 17:22:46 +0100

    Add functions in PostsPages: create/update/delete/get.
    Functions are linked to PostsApi, but not yet to frontend.

--- frontend/src/components/posts/PostsPage.js
+++ frontend/src/components/posts/PostsPage.js
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import PostsApi from '../../api/PostsApi'
 
 function PostsPage() {
+
+    const [ posts, setPosts ] = useState([])
+
+    function getAllPosts() {
+        PostsApi.getAllPosts()
+            .then((data) => {
+                setPosts(data);
+            })
+    }
+
+    function createPost() {
+        const newPost = {}; // Need to create this object with 'craeting form' values
+        PostsApi.createPost(newPost)
+            .then(() => {
+                getAllPosts(); // to refresh the list immediately
+            })
+    }
+
+    function deletePost(postId) {
+        PostsApi.deletePost(postId)
+            .then(() => {
+                getAllPosts(); // to refresh the list immediately
+            })
+    }
+
+    // Not a priority - What User Interface for updating post ?
+    function updatePost() {
+        const updatedPost = {}; // Need to create this object with 'updating form' values
+        PostsApi.updatePost(updatedPost)
+            .then(() => {
+                getAllPosts(); // to refresh the list immediately
+            })
+    }
+
+
     return (
         <div>
         </div>
