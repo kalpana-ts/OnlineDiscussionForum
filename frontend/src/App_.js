dd4f2db5979ad3b4c5b5567c46a1cffc412fd300
Sneha <sneha.gupta@gmail.com>
2020-11-17 20:56:15 +0100

    Modify js files to import css files

--- frontend/src/App_.js
+++ frontend/src/App_.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
+// Import custom styles for our application
 import './App.css';
 
+import Auth from './services/Auth';
+import Navbar from "./components/layout/Navbar";
+
+// Import pages
+import LoginPage from "./components/auth/LoginPage";
+import HomePage from './components/home/HomePage';
+import PostsPage from "./components/posts/PostsPage";
+import ChatPage from './components/chat/ChatPage';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [loggedIn, setLoggedIn] = useState(Auth.isLoggedIn());
+  Auth.bindLoggedInStateSetter(setLoggedIn);
+  
+  const loggedInRouter = (
+            <Router>
+                <Navbar onLogout={() => Auth.logout()} />
+
+                <div className="container mt-5">
+                    <Switch>
+                        <Route path="/posts">
+                            <PostsPage/>
+                        </Route>
+
+                        <Route path="/chat">
+                            <ChatPage/>
+                        </Route>
+
+                        <Route path="/">
+                          <HomePage/>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
   );
+
+  return (loggedIn ? loggedInRouter : <LoginPage/>);
 }
 
 export default App;
