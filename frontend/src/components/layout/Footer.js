4f6e35756e1ef9fd0ee580e481ca4d832d56f09b
Sneha <sneha.gupta@gmail.com>
2020-11-18 22:40:21 +0100

    Add fotter layout

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import './Footer.css';
+
+function Footer() {
+    return(
+        <footer class="contactUs">
+            <ul class="contactUs-list">
+                <li>Creators:@team-wd-9</li>
+                <li>
+                    <a href="https://www.community.com/privacy/explanation/">Privacy</a></li>
+                <li>
+                    <a href="https://www.community.com/policies?ref=pf/">Terms</a></li>
+                <li>
+                    <a href="https://www.community.com/policies/cookies/">Cookies</a></li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
