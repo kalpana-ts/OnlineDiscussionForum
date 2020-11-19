80c8fc406547ecbb57504010ea575b579f0c0bfe
Radha <radhasr1986@gmail.com>
2020-11-19 14:36:33 +0100

    password protection in user

--- src/main/java/se/kth/sda/skeleton/user/UserController.java
+++ src/main/java/se/kth/sda/skeleton/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import se.kth.sda.skeleton.auth.AuthService;
 
 import java.util.List;
 
@@ -14,10 +15,18 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthService authService;
 
     @GetMapping("")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/me")
+    public User getCurrentUser() {
+        String email=authService.getLoggedInUserEmail();
+        return userService.findUserByEmail(email);
     }
 
     @GetMapping("/{email}")
